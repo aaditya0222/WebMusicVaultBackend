10f1f9f6b3269f8a947ffa109168215dfb2f85a9
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2026-03-05 12:07:02 +0545

    Added Error Code in Support for sending responses

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,11 +1,21 @@
+import { ErrorCode } from "./ErrorCode";
+import { HttpStatus } from "./HttpStatus";
+
 class ApiError extends Error {
-  status: number;
+  status: typeof HttpStatus;
   errors?: string[];
-  constructor(status: number, message: string, errors?: string[]) {
+  code?: typeof ErrorCode;
+  constructor(
+    status: typeof HttpStatus,
+    message: string,
+    errors?: string[],
+    code?: typeof ErrorCode,
+  ) {
     super(message);
     this.name = "ApiError";
     this.status = status;
     this.errors = errors || undefined;
+    this.code = code;
     if (process.env.NODE_ENV === "development") {
       Error.captureStackTrace(this, this.constructor);
     }
