f2176d7ed99620bb629a4f93fa8cdd2940622ca5
Aaaaadddddiiii <chaurasiyarita20@gmail.com>
2026-03-14 15:28:48 +0545

    Test backend ci

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ const startServer = async (): Promise<void> => {
     await connectDb();
     app.listen(env.PORT);
     console.log("Successfully Started Server");
+    console.log("Test backend ci");
   } catch (error) {
     if (error instanceof Error) {
       console.error(
